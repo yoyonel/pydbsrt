3c13245cddfcec97f18affcac1f349100554641b
Lionel Atty <lionel.atty@365talents.com>
2019-08-03 23:27:48 +0200

    req: remove py-translator (seems to not exit anymore, todo: verify this package/req).

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "pysrt>=1.1.1",
         "python-Levenshtein>=0.12.0",
         "python-opensubtitles>=0.2.dev0",
-        "py-translator>=1.8.9",
+        # "py-translator>=1.8.9",
         "googletrans>=2.4.0",
         "tqdm>=4.24.0",
     ],
