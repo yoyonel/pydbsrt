49413baa27b36517a20e1a5c1078c59e4b95685b
Lionel Atty <lionel.atty@365talents.com>
2020-05-17 23:36:10 +0200

    refactoring + TODO.txt updated

--- src/pydbsrt/app.py
+++ src/pydbsrt/app.py
@@ -1,21 +1,23 @@
 """
 """
+import itertools
 import logging
+import operator
 from collections import defaultdict
 from hashlib import md5
 from itertools import islice
 from math import log, e
 from pathlib import Path
+from pprint import pformat
 from typing import Dict, Any, Union
 
 import bitstring
 import imageio
 import numpy as np
+from more_itertools import grouper
 from tqdm import tqdm
 
-from pydbsrt.tools.ffmpeg_tools.ffmeg_extract_frame import (
-    ffmpeg_imghash_generator
-)
+from pydbsrt.tools.ffmpeg_tools.ffmeg_extract_frame import ffmpeg_imghash_generator
 from pydbsrt.tools.imghash import imghash_to_bitarray
 from pydbsrt.tools.importantframefingerprint import ImportantFrameFingerprints
 from pydbsrt.tools.subfingerprint import SubFingerprints
@@ -46,22 +48,17 @@ def show_fingerprints(video_reader):
         pass
 
 
-def show_subtitles_fingerprints(video_reader, srt_path):
-    # map to group fingerprints by index subtitles (for convenience)
-    map_index_subtitles = defaultdict(int)
-    for index_subtitle, id_frame, fp in SubFingerprints(
-            subreader=SubReader(srt_path),
-            vfp=VideoFingerprint(video_reader),
-    ):
-        if map_index_subtitles[index_subtitle] == 0:
-            print(
-                f"\nindex subtitle: {index_subtitle} - first frame: {id_frame}",
-                end='')
-        if map_index_subtitles[index_subtitle] % 8 == 0:
-            print("")
-        # https://stackoverflow.com/questions/493386/how-to-print-without-newline-or-space
-        print(f"{fp} ", end='')
-        map_index_subtitles[index_subtitle] += 1
+def show_subtitles_fingerprints(video_reader: VideoReader, srt_path: Path) -> None:
+    nb_fingerprints_by_chunk = 5
+    it_subfingerprints = SubFingerprints(subreader=SubReader(srt_path), vfp=VideoFingerprint(video_reader))
+    gb_subfingerprints = itertools.groupby(it_subfingerprints, key=operator.itemgetter(0))
+    for index_subtitle, it_indexed_subfingerprints in gb_subfingerprints:
+        _, id_frame, fingerprint = next(it_indexed_subfingerprints)
+        it_indexed_subfingerprints = itertools.chain(((index_subtitle, id, fingerprint),), it_indexed_subfingerprints)
+        print(f"* index subtitle: {index_subtitle} - first frame: {id_frame}")
+        for chunk_fingerprints in grouper((fingerprint for _, __, fingerprint in it_indexed_subfingerprints),
+                                          nb_fingerprints_by_chunk):
+            print(" ".join(map(str, filter(None, chunk_fingerprints))))
 
 
 def entropy2(labels, base=e):
@@ -135,15 +132,15 @@ def export_fingerprints(input_media_path: Path) -> Path:
     => ~ x25.675
 
     """
-    export_path = Path('/tmp/imghash')
+    export_path = Path('/tmp/img_hash')
     export_path.mkdir(exist_ok=True)
     # https://stackoverflow.com/questions/38175170/python-md5-cracker-typeerror-object-supporting-the-buffer-api-required
     export_fp = export_path.joinpath(
         f"{md5(str(input_media_path).encode()).hexdigest()}.ba")
     with open(export_fp, 'wb') as fp:
-        for imghash in tqdm(ffmpeg_imghash_generator(str(input_media_path))):
-            ba_imghash = imghash_to_bitarray(imghash)
-            fp.write(ba_imghash.bytes)
+        for img_hash in tqdm(ffmpeg_imghash_generator(str(input_media_path))):
+            ba_img_hash = imghash_to_bitarray(img_hash)
+            fp.write(ba_img_hash.bytes)
     return export_fp
 
 
@@ -156,59 +153,53 @@ def import_fingerprints(input_fingerprints_path: Path) -> hex:
     chunk_nb_frames = 2048  # for 8k of fingerprints
     chunk_nb_bytes_to_read = chunk_nb_frames << 3  # * 8
     # https://stackoverflow.com/questions/1035340/reading-binary-file-and-looping-over-each-byte
-    print(f"Reading imghash file: {input_fingerprints_path} ...")
+    print(f"Reading img_hash file: {input_fingerprints_path} ...")
     # with open(input_fingerprints_path, "rb") as f:
     with input_fingerprints_path.open('rb') as f:
         while True:
             chunk = f.read(chunk_nb_bytes_to_read)
-            chunk_nb_imghash = len(chunk) >> 3  # // 8
-            if chunk_nb_imghash:
+            chunk_nb_img_hash = len(chunk) >> 3  # // 8
+            if chunk_nb_img_hash:
                 # https://pythonhosted.org/bitstring/packing.html#compact-format
-                for int64_imghash in bitstring.BitArray(chunk).unpack(
-                        fmt=f'>{chunk_nb_imghash}Q'):
-                    yield hex(int64_imghash)
+                for int64_img_hash in bitstring.BitArray(chunk).unpack(fmt=f'>{chunk_nb_img_hash}Q'):
+                    yield hex(int64_img_hash)
             else:
                 break
 
 
 def main():
-    bbbt = medias_path['big_buck_bunny_trailer_480p']
-    media_path = bbbt['media']  # type: Path
-    st_path = bbbt['subtitles']  # type: Path
+    media = medias_path['big_buck_bunny_trailer_480p']
+    media_path = media['media']  # type: Path
+    srt_path = media['subtitles']  # type: Path
 
     logger.info(f"media path: {media_path}")
-    logger.info(f"subtitles path: {st_path}")
+    logger.info(f"subtitles path: {srt_path}")
 
     video_reader = VideoReader(media_path)
-    logger.info(f"Video reader meta datas: {video_reader.metadatas}")
+    logger.info(f"Video reader meta data:\n{pformat(video_reader.metadatas)}")
 
     # show_fingerprints(video_reader)
     #
-    show_subtitles_fingerprints(video_reader, srt_path=st_path)
+    show_subtitles_fingerprints(video_reader, srt_path=srt_path)
 
     # show_important_frames_fingerprints(video_reader, threshold_distance=4)
 
     fp_exported = export_fingerprints(media_path)
     logger.info(f"Fingerprints exported: {fp_exported}")
 
-    map_imghash_occurency = defaultdict(int)
-    for imghash in import_fingerprints(fp_exported):
-        map_imghash_occurency[imghash] += 1
-    logger.info(f"Nb distinct imghash: {len(list(map_imghash_occurency.keys()))}")
+    map_img_hash_occurency = defaultdict(int)
+    for img_hash in import_fingerprints(fp_exported):
+        map_img_hash_occurency[img_hash] += 1
+    logger.info(f"Nb distinct img_hash: {len(list(map_img_hash_occurency.keys()))}")
 
     # instance.opt
     nb_fingerprints = 25 * 2  # 1 minute
-    with open("/tmp/imghash/instance.opt", "w") as fp:
-        imghashs = set(islice(import_fingerprints(fp_exported), nb_fingerprints))
-        print(
-            "2\n"
-            f"{len(imghashs)}\n"
-            "64\n"
-            "0\n"
-            "1", file=fp)
-        for imghash in imghashs:
+    with open("/tmp/img_hash/instance.opt", "w") as fp:
+        img_hashs = set(islice(import_fingerprints(fp_exported), nb_fingerprints))
+        print("\n".join(("2", str(len(img_hashs)), "64", "0", "1")), file=fp)
+        for img_hash in img_hashs:
             # https://stackoverflow.com/questions/1425493/convert-hex-to-binary
-            print(bin(int(imghash, 16))[2:], file=fp)
+            print(bin(int(img_hash, 16))[2:], file=fp)
 
 
 def init_logger():
