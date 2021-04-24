5710d60f96918edd37cce94814ee1e3237ddde9c
ATTY Lionel <yoyonel@hotmail.com>
2021-04-24 18:23:21 +0200

    fix utests + add submodule to cBKTree

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ def resource_video_path():
     https://stackoverflow.com/a/47704630 (How to generate a 2hour-long blank video)
     ffmpeg commands:
         - `ffmpeg -y -t 1 -f lavfi -i color=c=white:s=32x32 -c:v libx264 -tune stillimage -pix_fmt yuv420p white_frames.mp4`
-        - `ffmpeg -y -t 1 -f lavfi -i color=c=black:s=32x32 -c:v libx264 -tune stillimage -pix_fmt yuv420p white_frames.mp4`
+        - `ffmpeg -y -t 1 -f lavfi -i color=c=black:s=32x32 -c:v libx264 -tune stillimage -pix_fmt yuv420p black_frames.mp4`
     TODO: tester d'autres encodages !
     """
     return lambda color_frame: Path(f"data/{color_frame}_frames.mp4")
